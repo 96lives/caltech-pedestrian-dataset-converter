58c0c6a15a3d6f2dae73739b7f49cf00f6c6348d
96lives <96lives@gmail.com>
2018-11-08 14:12:39 +0900

    convert_seq.py to set_name and refactoring

--- convert_seqs.py
+++ convert_seqs.py
@@ -7,48 +7,40 @@ set 07~10: test set
 
 import os
 import glob
-import cv2 as cv
+import cv2
 
+def save_img(frame, out_dir, file_name, frame_num):
+    full_file_name = os.path.join(out_dir, file_name + '_' + \
+                                  str(frame_num).zfill(6) + ".jpg")
+    cv2.imwrite(full_file_name, frame)
+
+'''
 def save_img(out_dir_set, set_name, file_name, frame_num ,frame):
     cv.imwrite('{}/{}_{}_{}.jpg'.format(
         out_dir_set, set_name, file_name, str(frame_num).zfill(5)),
         frame)
+'''
 
-def convert_seqs(out_dir, config):
-    train_dir = os.path.join(out_dir, "train")
-    test_dir = os.path.join(out_dir, "test")
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir)
-    if not os.path.exists(train_dir):
-        os.makedirs(train_dir)
-    if not os.path.exists(test_dir):
-        os.makedirs(test_dir)
+def convert_seqs(dataset, config):
+
+    seq_dir = config["seq_dir"]
+    img_dir = config["img_dir"]
+    os.makedirs(img_dir, exist_ok=True)
 
     img_cnt = 1
-    for dname in sorted(glob.glob('data/set*')):
+    for dname in sorted(glob.glob(os.path.join(seq_dir, "set*"))):
         set_name = dname.split("/")[1]
-        train_set = config["TRAIN_SET"]
-        test_set = config["TEST_SET"]
-
-        if set_name in ["set"+str(x).zfill(2) for x in train_set]:
-            out_dir_set = train_dir
-        elif set_name in ["set"+str(x).zfill(2) for x in test_set]:
-            out_dir_set = test_dir
-        else:
-            continue
-
-        if not os.path.exists(out_dir_set):
-            os.makedirs(out_dir_set)
+        out_dir = os.path.join(img_dir, set_name)
+        os.makedirs(out_dir_set, exist_ok=True)
 
         for fn in sorted(glob.glob('{}/*.seq'.format(dname))):
-            _, set_name, file_name = fn.split("/")
-            file_name = file_name.split(".")[0]
-            cap = cv.VideoCapture(fn)
+            file_name = fn.split('/')[-1].split(".")[0]
+            cap = cv2.VideoCapture(fn)
             frame_num = 0
             while True:
                 ret, frame = cap.read()
                 if not ret:
                     break
-                save_img(out_dir_set, set_name, file_name, frame_num, frame)
+                save_img(frame, out_dir, file_name, fram_num)
                 frame_num += 1
             print(fn)
