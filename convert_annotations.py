917f755eec85817e0b9e341888a71dbb127a9ad4
96lives <96lives@gmail.com>
2018-11-23 15:51:58 +0900

    to train_with labels

--- convert_annotations.py
+++ convert_annotations.py
@@ -3,6 +3,7 @@ import cv2
 import json, yaml
 import pdb
 import yaml
+from shutil import copyfile
 from PIL import Image
 from glob import glob
 from scipy.io import loadmat
@@ -11,9 +12,9 @@ class VBBConverter():
     def __init__(self, dataset, config):
         config = config[dataset]
         vbb_dir = config["vbb_dir"]
-        img_dir = config["img_dir"]
+        self.img_dir = config["img_dir"]
         ann_dir = config["ann_dir"]
-        if not os.path.exists(img_dir):
+        if not os.path.exists(self.img_dir):
             print("image path does not exist")
             return
         if not os.path.exists(vbb_dir):
@@ -22,14 +23,16 @@ class VBBConverter():
         os.makedirs(ann_dir, exist_ok=True)
         self._set_basic_info()
         self._set_categories()
-
+        self.img_save_path = os.path.join(self.img_dir, "test_with_labels")
+        os.makedirs(self.img_save_path, exist_ok=True)
         for set_dir in sorted(glob(os.path.join(vbb_dir, "set*"))):
             set_name = set_dir.split("/")[-1]
-            self._get_images(img_dir, set_name)
-            self._get_annotation(set_dir)
-            json_data = self._get_json_format()
-            with open(os.path.join(ann_dir, set_name + ".json"), "w") as jsonfile:
-                json.dump(json_data, jsonfile, sort_keys=True, indent=4)
+            self._get_annotation(set_dir) # sets self.annotations and self.images
+            print(set_dir)
+        with open(os.path.join(ann_dir,"test.json"), "w") as jsonfile:
+            json.dump(self._get_json_format(), jsonfile, sort_keys=True, indent=4)
+        print(len(self.images))
+        print(len(self.annotations))
 
     def _set_basic_info(self):
         self.info = {"year" : 2009,
@@ -44,70 +47,58 @@ class VBBConverter():
                           "url": "http://creativecommons.org/licenses/by-nc-sa/2.0/"
                          }]
         self.type = "instances"
+        self.annotations = []
+        self.images = []
+        self.id_cnt = 1
+        self.w = 640
+        self.h = 512
 
     def _set_categories(self):
-        with open("categories.json") as json_data:
+        with open("kaist_categories.json") as json_data:
             self.categories = json.load(json_data)["categories"]
-        self.cat2id = {"person": 1, "people": 2}
-
-    def _get_images(self, img_dir, set_name):
-        img_dir = os.path.join(img_dir, set_name)
-        file_names = sorted(os.listdir(img_dir))
-        if len(os.listdir(img_dir)) == 0:
-            return []
-        w, h = self._get_img_size(img_dir, file_names[0])
-        self.images =  self._img_list_to_dict(img_dir, file_names, w, h)
-
-    def _get_img_size(self, img_dir, img_name):
-        img = Image.open(os.path.join(img_dir, img_name))
-        w, h = img.size
-        img.close()
-        return w, h
-
-    def _img_list_to_dict(self, img_dir, file_names, w, h):
-        id_cnt = 1
-        res = []
-        for f in file_names:
-            res.append({
-                "date_captured" : "2009",
-                "file_name" : f,
-                "id" : id_cnt,
-                "license" : 1,
-                "url" : "",
-                "height" : h,
-                "width" : w
-            })
-            id_cnt += 1
-        return res
+            self.cat2id = {"person": 1, "person?": 2, "people": 3, "cyclist": 4}
 
     def _get_annotation(self, set_dir):
-        total_frame_cnt = 1
-        annotations = []
+        set_name = set_dir.split('/')[-1]
         for anno_fn in sorted(glob(os.path.join(set_dir, "*.vbb"))):
             vbb = loadmat(anno_fn)
             nFrame = int(vbb['A'][0][0][0][0][0]) # number of frames
-            objLists = vbb['A'][0][0][1][0]
-            annots = self._get_bboxes(objLists, total_frame_cnt)
-            annotations.extend(annots)
-            total_frame_cnt += nFrame
-        self.annotations = annotations
-
-    def _get_bboxes(self, objLists, total_frame_cnt):
-        annotations = []
-        for frame_id, obj in enumerate(objLists):
-            obj = obj[0]
-            for bbox in obj:
-                bbox = bbox[1]
-                annotations.append({
+            obj_lists = vbb['A'][0][0][1][0]
+            obj_lbl = [str(v[0]) for v in vbb['A'][0][0][4][0]]
+            for frame_id, objs in enumerate(obj_lists):
+                labels_exist = False
+                bboxes = []
+                for obj in objs[0]:
+                    category = obj_lbl[obj[0][0][0]-1] # add class name to bbox
+                    bbox = obj[1][0] # add x, y, w, h to bbox
+                    self.annotations.append({
                         "segmentation": [],
                         "area": 0,
                         "iscrowd": 0,
-                        "image_id" : frame_id + total_frame_cnt,
+                        "image_id" : self.id_cnt,
                         "bbox" : bbox.tolist()[0],
-                        "category_id" : 1,
-                        "id": frame_id + total_frame_cnt
+                        "category_id" : self.cat2id[category],
+                        "id": self.id_cnt
                     })
-        return annotations
+                    labels_exist = True
+                if labels_exist:
+                    vbb_name = anno_fn.split('/')[-1].split('.')[0]
+                    old_file_name = vbb_name + '_I' + str(frame_id).zfill(5)+'.jpg'
+                    new_file_name = set_name +'_' + old_file_name
+                    self.images.append({
+                                "date_captured" : "2009",
+                                "file_name" : new_file_name,
+                                "id" : self.id_cnt,
+                                "license" : 1,
+                                "url" : "",
+                                "height" : self.h,
+                                "width" : self.w
+                            })
+                    img_path = os.path.join(self.img_dir, set_name, old_file_name)
+                    out_path = os.path.join(self.img_save_path, new_file_name)
+                    copyfile(img_path, out_path)
+                    self.id_cnt += 1
+
 
     def _get_json_format(self):
          return {
@@ -118,19 +109,3 @@ class VBBConverter():
                 "annotations" : self.annotations,
                 "categories" : self.categories
          }
-'''
-        for frame_id, obj in enumerate(objLists):
-            if len(obj) > 0:
-                for id, pos, occl, lock, posv in zip(
-                        obj['id'][0], obj['pos'][0], obj['occl'][0],
-                        obj['lock'][0], obj['posv'][0]):
-                    annotations.append({
-                        "segmentation": [],
-                        "area": 0,
-                        "iscrowd": 0,
-                        "image_id" : frame_id + total_frame_cnt,
-                        "bbox" : pos.tolist()[0],
-                        "category_id" : 1,
-                        "id": frame_id + total_frame_cnt
-                    })'''
-
